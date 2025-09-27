c90ac9b8e14f8ba2c01423045865a171fde24a61
jd117101 <madicatanzaro@gmail.com>
2025-09-27 13:54:38 +1000

    Update score file using a function and include random values with results.

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,27 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+from random import randint
+
+
+def main():
+    user_score = float(input("Enter score: "))
+    user_result = determine_result(user_score)
+    print(f"Result: {user_result}")
+    random_score = randint(0, 100)
+    random_result = determine_result(random_score)
+    print(f"Random score of {random_score} with result: {random_result}")
+
+
+def determine_result(user_score):
+    if user_score < 0 or user_score > 100:
+        return "Invalid score"
+    elif user_score >= 90:
+        return "Excellent"
+    elif user_score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
