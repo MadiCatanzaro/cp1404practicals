781d1f86ebaacc3ab00c4822ecec054e495da0c9
jd117101 <madicatanzaro@gmail.com>
2025-10-05 13:55:14 +1000

    Add completed randoms file

---
+++ prac_03/randoms.py
@@ -0,0 +1,25 @@
+"""
+Randoms
+"""
+
+import random
+
+# Question 1
+# What did you see on line 1?
+# The smallest number I saw was 7 and the largest was 19.
+# The smallest number which could have been seen is 5 and the largest 20.
+
+# Question 2
+# What did you see on line 2?
+# I saw a lot of 7's, and one 5 and one 9.
+# The smallest number I could have seen was 3 and the largest was 9.
+# The line could not have produced a 4 and the numbers are increasing in multiples of 2,
+# and therefore only the odd numbers can be shown.
+
+# Question 3
+# What did you see on line 3?
+# I saw many different numbers with lots of decimal places, varying from approximately 2.5 to 5.
+# The smallest number I could have seen is 2.5 and the largest is 5.5.
+
+random_number = random.randint(1, 100)
+print(random_number)
