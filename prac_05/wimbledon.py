ac2f48d0b589aba6816e983ce137d7abccd38ef1
jd117101 <madicatanzaro@gmail.com>
2025-10-18 15:41:20 +1000

    Add function docstrings to all functions in Wimbledon file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,11 +1,12 @@
 """
 Wimbledon
 Estimate: 40 minutes
-Actual: 42 minutes
+Actual: 45 minutes
 """
 
 
 def main():
+    """Produce the winning champions and countries of Wimbledon."""
     champion_to_times_won = {}
     countries = []
     with open('wimbledon.csv', "r", encoding='utf-8-sig') as in_file:
@@ -19,6 +20,7 @@ def main():
 
 
 def count_winning_players(champion_to_times_won, wimbledon):
+    """Count how many times a player has won Wimbledon."""
     player = wimbledon[2]
     if player in champion_to_times_won:
         champion_to_times_won[player] += 1
@@ -27,18 +29,21 @@ def count_winning_players(champion_to_times_won, wimbledon):
 
 
 def get_countries(wimbledon, countries):
+    """Return a list of all the countries who have won Wimbledon."""
     country = wimbledon[1]
     countries.append(country)
     return countries
 
 
 def display_champions(champion_to_times_won):
+    """Display each champion and the corresponding amount of times they have won Wimbledon."""
     print("Wimbledon Champions:")
     for champion, times_won in champion_to_times_won.items():
         print(f"{champion} {times_won}")
 
 
 def display_winning_countries(countries):
+    """Display which countries have won, and how many countries in total have won Wimbledon"""
     print("")  # blank line
     winning_countries = sorted(set(countries))
     print(f"These {len(winning_countries)} countries have won Wimbledon:")
