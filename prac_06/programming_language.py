94edb18ca9d478cafc2f18acb570d67b5078e23b
jd117101 <madicatanzaro@gmail.com>
2025-11-02 21:41:38 +1000

    Add complete programming language file

---
+++ prac_06/programming_language.py
@@ -0,0 +1,27 @@
+"""
+Programming Language
+Estimate: 25 minutes
+Actual: so far 23 minutes
+"""
+
+
+class ProgrammingLanguage:
+    """Represent the Programming Language Class."""
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Initialise Programming Language class attributes; name, typing, reflection, year."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """String output to return"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Return true if language is dynamic, false if static."""
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
