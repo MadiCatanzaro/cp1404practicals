7221d0a673d0e8b72878db6f4f3d4c2a2c12d09a
jd117101 <madicatanzaro@gmail.com>
2025-11-02 22:14:24 +1000

    Update and correct formatting in guitars file

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,7 @@
 """
 Guitars
 """
+
 from guitar import Guitar
 
 print("My Guitars!")
@@ -14,9 +15,7 @@ while name != "":
     print(Guitar(name, year, cost), "added")
     name = input("Name: ")
 
-
 print("These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     vintage_string = "(vintage)" if guitar.is_vintage() else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage_string}")
-
