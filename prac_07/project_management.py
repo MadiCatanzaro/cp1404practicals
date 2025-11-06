e2fc22668f5969cebbfc0667e866057d4d3ed498
jd117101 <madicatanzaro@gmail.com>
2025-11-06 15:56:08 +1000

    Add basic structure to project management file

---
+++ prac_07/project_management.py
@@ -0,0 +1,41 @@
+"""
+Project Management
+Estimate: 2 hours
+Actual:
+"""
+
+MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit
+"""
+
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu option")
+            print(MENU)
+            choice = input(">>> ").upper()
+
+
+
+main()
