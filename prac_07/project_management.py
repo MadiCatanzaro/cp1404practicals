26e5139634b74d3169d8faee434013a1bb0ad63a
jd117101 <madicatanzaro@gmail.com>
2025-11-07 14:50:29 +1000

    Add Project class

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,23 @@ Estimate: 2 hours
 Actual:
 """
 
+
+class Project:
+
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.00, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage}"
+
+    def __repr__(self):
+        return f"({self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage})"
+
+
 MENU = """- (L)oad projects
 - (S)ave projects
 - (D)isplay projects
@@ -15,12 +32,15 @@ MENU = """- (L)oad projects
 
 
 def main():
+    projects = []
     print("Welcome to Pythonic Project Management")
+    load_file("projects.txt")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            load_file("projects.txt")
+            print("stop")
         elif choice == "S":
             pass
         elif choice == "D":
@@ -36,6 +56,13 @@ def main():
             print(MENU)
             choice = input(">>> ").upper()
 
+
+def load_file(filename):
+    in_file = open(filename, "r")
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split('\t')
+        print(parts)
 
 
 main()
