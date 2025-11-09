f14910493f278215918fc10b7371498c0847e4ff
jd117101 <madicatanzaro@gmail.com>
2025-11-09 17:49:22 +1000

    Add complete files from Project Management task (project management, projects)

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,7 +1,7 @@
 """
 Project Management
 Estimate: 2 hours
-Actual:
+Actual: 1:56 so far
 """
 
 import datetime
@@ -9,8 +9,8 @@ from operator import attrgetter
 
 
 class Project:
-
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise Class objects."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -18,15 +18,22 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
+        """Display string representation of Project."""
         return (
-            f"{self.name}, start: {self.start_date.strftime("%d/%m/%Y")}, priority {self.priority}, estimate: ${self.cost_estimate:.2f},"
-            f" completion: {self.completion_percentage}%")
+            f"{self.name}, start: {self.start_date.strftime("%d/%m/%Y")}, priority {self.priority}, estimate: "
+            f"${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
 
     def __repr__(self):
-        return f"({self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage})"
+        """Display string representation of Project."""
+        return f"({self.name}, {self.start_date.strftime("%d/%m/%Y")}, {self.priority}, {self.cost_estimate}, {self.completion_percentage})"
 
     def __lt__(self, other):
+        """Return true if self is less than other."""
         return self.priority < other.priority
+
+    def is_complete(self):
+        """Return true if completion percentage is 100."""
+        return self.completion_percentage == 100
 
 
 MENU = """- (L)oad projects
@@ -35,35 +42,35 @@ MENU = """- (L)oad projects
 - (F)ilter projects by date
 - (A)dd new project
 - (U)pdate project
-- (Q)uit
-"""
+- (Q)uit"""
 
 
 def main():
+    """Main function for Project Management."""
     print("Welcome to Pythonic Project Management")
     projects = load_file("projects.txt")
-    print(projects)
-    projects.sort(key=attrgetter("priority"))
-    print(sorted(projects, key=attrgetter("completion_percentage")))
     print(f"Loaded {(len(projects))} from projects.txt")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            filename = input("File to load: ")
-            load_file(filename)
-            print(f"{filename} loaded")
+            try:
+                filename = input("File to load: ")
+                load_file(filename)
+                print(f"{filename} loaded")
+            except FileNotFoundError:
+                print("File not found")
         elif choice == "S":
             filename = input("File to save: ")
             save_to_file(filename, projects)
         elif choice == "D":
-            incomplete, complete = (test_complete_projects(projects))
+            incomplete, complete = (separate_complete_projects(projects))
             display_projects(incomplete, complete)
         elif choice == "F":
             sort_by_date(projects)
         elif choice == "A":
             add_project(projects)
-            print(projects)
+            # print(projects)
         elif choice == "U":
             update_projects(projects)
         else:
@@ -74,9 +81,17 @@ def main():
 
 
 def sort_by_date(projects):
+    """Sort projects by date after a given date."""
     current_projects = []
-    user_date_string = input("Show projects that start after date (d/m/yyyy): ")
-    user_date = datetime.datetime.strptime(user_date_string, "%d/%m/%Y").date()
+    is_valid = False
+    while not is_valid:
+        try:
+            user_date_string = input("Show projects that start after date (dd/mm/yyyy): ")
+            user_date = datetime.datetime.strptime(user_date_string, "%d/%m/%Y").date()
+            is_valid = True
+        except ValueError:
+            print("Invalid date input")
+
     for project in projects:
         if project.start_date > user_date:
             current_projects.append(project)
@@ -85,25 +100,43 @@ def sort_by_date(projects):
 
 
 def update_projects(projects):
+    """Update percentage or priority of project."""
     for i, project in enumerate(projects):
         print(f"{i} {project}")
-    project_to_modify = int(input("Project choice: "))
+    is_valid = False
+    while not is_valid:
+        try:
+            project_to_modify = int(input("Project choice: "))
+            is_valid = True
+        except IndexError:  # does not handle IndexErrors
+            print("Invalid project choice, must be chosen from the given project numbers")
+        except ValueError:
+            print("Invalid project choice, please enter a number")
     print(projects[project_to_modify])
-    percentage = int(input("New Percentage: "))
-    priority = int(input("New Priority: "))
+    try:
+        percentage = int(input("New Percentage: "))
+    except ValueError:
+        percentage = projects[project_to_modify].percentage
+    try:
+        priority = int(input("New Priority: "))
+    except ValueError:
+        priority = projects[project_to_modify].priority
+
     projects[project_to_modify].completion_percentage = percentage
     projects[project_to_modify].priority = priority
 
 
 def quit_program(projects):
-    quitting_save_option = input("Would you like to save to projects.txt? (Y/N) ").upper
+    """Quit program, optionally saving projects to project.txt file."""
+    quitting_save_option = input("Would you like to save to projects.txt? (Y/N) ").upper()
     if quitting_save_option == 'Y':
         save_to_file("project.txt", projects)
-    else:
-        print("Thank you for using custom-built project management software.")
+        print("Projects saved.")
+    print("Thank you for using custom-built project management software.")
 
 
 def save_to_file(filename, projects):
+    """Save projects to specified file."""
     save_file = open(filename, "w")
     for project in projects:
         print(project, file=save_file)
@@ -111,17 +144,26 @@ def save_to_file(filename, projects):
 
 
 def add_project(projects):
+    """Add new project to projects."""
+    is_valid = False
     print("Let's add a new project")
-    name = input("Name: ")
-    start_date_string = input("Start date (d/m/yyyy): ")
-    date = datetime.datetime.strptime(start_date_string, "%d/%m/%Y").date()
-    priority = int(input("Priority: "))
-    cost_estimate = float(input("Cost estimate: "))
-    completion_percentage = int(input("Completion percentage: "))
+    while not is_valid:
+        try:
+            name = input("Name: ")
+            start_date_string = input("Start date (dd/mm/yyyy): ")
+            date = datetime.datetime.strptime(start_date_string, "%d/%m/%Y").date()
+            priority = int(input("Priority: "))
+            cost_estimate = float(input("Cost estimate: $"))
+            completion_percentage = int(input("Completion percentage: "))
+            is_valid = True
+
+        except ValueError:
+            print("Invalid project input")
     projects.append(Project(name, date, priority, cost_estimate, completion_percentage))
 
 
 def load_file(filename):
+    """Load projects from file and process into an appropriate format."""
     projects = []
     in_file = open(filename, "r")
     in_file.readline()
@@ -137,11 +179,12 @@ def load_file(filename):
     return projects
 
 
-def test_complete_projects(projects):
+def separate_complete_projects(projects):
+    """Separate files into complete and incomplete."""
     complete_projects = []
     incomplete_projects = []
     for project in projects:
-        if project.completion_percentage == 100:
+        if project.is_complete():
             complete_projects.append(project)
         else:
             incomplete_projects.append(project)
@@ -149,10 +192,11 @@ def test_complete_projects(projects):
 
 
 def display_projects(incomplete_projects, complete_projects):
+    """Display incomplete and complete projects."""
     print("Incomplete projects:")
     for project in sorted(incomplete_projects):
         print(f"\t{project}")
-    print("Complete projects:")
+    print("Completed projects:")
     for project in sorted(complete_projects):
         print(f"\t{project}")
 
