f276107ea761292b5313c479ff4cf7bbc6f73dfd
jd117101 <madicatanzaro@gmail.com>
2025-11-23 21:26:32 +1000

    Add complete taxi class and taxi test files

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,16 @@
+"""
+Taxi Test
+"""
+
+from taxi import Taxi
+
+my_taxi = Taxi('Prius 1', 100)
+my_taxi.drive(40)
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+# Restart the meter
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
